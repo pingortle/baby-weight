28b5ee22ce9cd98a498bdfa1047c9fc90861a7b5
Kaleb Lape <kaleb.lape@metova.com>
2017-04-08 20:16:51 -0500

    Refactor out stupidity.

--- imports/ui/components/weight-converter/pound-ounce-input/pound-ounce-input.js
+++ imports/ui/components/weight-converter/pound-ounce-input/pound-ounce-input.js
@@ -1,38 +1,40 @@
 import '../weight-input'
 import './pound-ounce-input.html'
 
+const POUND_FACTOR = 0.00220462
+const OUNCE_FACTOR = 0.035274
+const OUNCES_IN_POUND = 16
+
 Template.poundOunceInput.viewmodel({
-  gramValue: 0,
-  // gramValue: function (value) {
-  //   if(_.isUndefined(value))
-  //   {
-  //     console.log(this.poundInput(), this)
-  //     const gramValue = this.poundInput().gramValue() + this.ounceInput().gramValue()
-  //     console.log(`retreiving gramValue ${gramValue}`)
-  //     return gramValue
-  //   }
-  // },
+  poundValue: 0,
+  ounceValue: 0,
   hasFocus: function () {
-    const hasFocus = this.poundInput.hasFocus() || this.ounceInput.hasFocus()
-    console.log(`retreiving hasFocus ${hasFocus}`)
+    const hasFocus = this.poundInputHasFocus() || this.ounceInputHasFocus()
     return hasFocus
   },
   autorun: [
     function () {
-      const gramValue = this.gramValue()
-      console.log(`setting parent gramValue: ${gramValue}`)
+      const gramValue = this.poundValue() / POUND_FACTOR + this.ounceValue() / OUNCE_FACTOR
+      console.log(`poundOunceInput - setting parent gramValue: ${gramValue}`)
       this.parent().gramValue(gramValue)
     },
     function () {
       const gramValue = this.parent().gramValue()
       if (!this.hasFocus()) {
-        const pounds = Math.floor(gramValue * this.poundInput.gramFactor())
-        console.log(`setting pounds: ${pounds}`)
-        this.poundInput.value(pounds)
+        const pounds = Math.floor(gramValue * POUND_FACTOR)
+        console.log(`poundOunceInput - setting pounds: ${pounds}`)
+        this.poundValue(pounds)
 
-        const ounces = gramValue * this.ounceInput.gramFactor() % 16
-        console.log(`setting ounces: ${ounces}`)
-        this.ounceInput.value(ounces)
+        const ounces = Math.round(gramValue * OUNCE_FACTOR) % OUNCES_IN_POUND
+        console.log(`poundOunceInput - setting ounces: ${ounces}`)
+        this.ounceValue(ounces)
+      }
+    },
+    function () {
+      const ounces = this.ounceValue()
+      if(ounces >= OUNCES_IN_POUND) {
+        this.ounceValue(ounces % OUNCES_IN_POUND)
+        this.poundValue(this.poundValue.value + 1)
       }
     }
   ]
--- imports/ui/components/weight-converter/weight-input/weight-input.js
+++ imports/ui/components/weight-converter/weight-input/weight-input.js
@@ -3,17 +3,24 @@ import './weight-input.html'
 Template.weightInput.viewmodel({
   value: 0,
   hasFocus: false,
-  gramValue: function () {
-    return this.value() / this.gramFactor()
+  gramValue: function (grams) {
+    if (_.isUndefined(grams)) {
+      return this.value() / this.gramFactor()
+    } else {
+      this.value(grams * this.gramFactor())
+      return grams
+    }
   },
   autorun: [
     function () {
       const gramValue = this.gramValue()
+      console.log(`weightInput - setting parent gramValue: ${gramValue}`)
       this.parent().gramValue(gramValue)
     },
     function () {
       if (!this.hasFocus()) {
         const value = this.parent().gramValue() * this.gramFactor()
+        console.log(`weightInput - setting value: ${value}`)
         this.value(value)
       }
     }

a2b29fbd7c2aa7937cc8ad14674a8411fb7d4578
Kaleb Lape <kaleb.lape@metova.com>
2017-04-13 12:40:43 -0500

    Show latest weight in document title.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,10 @@
 import { Template } from 'meteor/templating'
 import { ReactiveVar } from 'meteor/reactive-var'
+import { Tracker } from 'meteor/tracker'
 
 import '/imports/ui/components/weight-converter'
 import '/imports/ui/components/kenny-weight'
+
+import { KennyWeights } from '/imports/collections/kenny-weights'
+
+Tracker.autorun(() => document.title = `Baby Weight - Latest: ${ KennyWeights.latest().grams }`)
